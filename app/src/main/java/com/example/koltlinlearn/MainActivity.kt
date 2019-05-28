3918e3dfc639e10628a7fe35a013af5820d3d6d0
funnyjerry <wenhu95piao@gmail.com>
2019-05-28 15:57:20 +0800

    add activity function

--- app/src/main/java/com/example/koltlinlearn/MainActivity.kt
+++ app/src/main/java/com/example/koltlinlearn/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.koltlinlearn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButon: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButon = findViewById<Button>(R.id.tap_me_button)
+        gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
+        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
     }
 }
